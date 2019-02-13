4f8bd207e0d75de9a1d675ef5dc248ccdba9981e
Nick Shorter <nickshorter@comcast.net>
2019-02-12 23:13:34 -0600

    update

--- Kyle/src/Album.java
+++ Kyle/src/Album.java
@@ -1,19 +1,19 @@
-import sun.jvm.hotspot.jdi.ArrayTypeImpl;
 
+enum genre {
+    RAP, ROCK, COUNTRY, POP
+}
 public class Album {
 
     public String Title;
     public String artist;
     public int releaseYear;
-    public enum MusicGenre {
-        RAP, ROCK, POP, COUNTRY;
-    }
-
+    public genre genre;
 
     public Album(String Title, String artist, int releaseYear) {
         this.Title = Title;
         this.artist = artist;
         this.releaseYear = releaseYear;
+        this.genre = null;
     }
 
     public String getTitle() {
@@ -26,5 +26,10 @@ public class Album {
     public int getReleaseYear(){
         return releaseYear;
     }
-
+    public genre getGenre() {
+        return genre;
+    }
+    public String toString() {
+        return "Your title" + Title + " artist" + artist + " ReleaseYear " + releaseYear + "Genre" + genre;
+    }
 }
--- Kyle/src/Testing.java
+++ Kyle/src/Testing.java
@@ -4,16 +4,33 @@ import java.util.*;
 
 public class Testing {
     public static void main(String[] args) throws IOException {
+
+        Album alb = new Album("Thunderstruck", "ACDC", 1983);
+
+        System.out.println(alb.toString());
+
         Scanner fin = new Scanner(new File("database.txt"));
 
+        int count = 0;
+        while (fin.hasNextLine()) {
+            count = count + 1;
+            fin.nextLine();
+        }
+        fin.close();
+        System.out.println(count);
 
-        String count = fin.nextLine();
+        String[] myArray = new String[count];
+        Scanner fin1 = new Scanner(new File("database.txt"));
+        for(int index=0; index < myArray.length; index ++) {
+            myArray[index] = fin1.nextLine();
 
 
-        for(int index =0; index <= count.length(); index++) {
-           String[] array = new String[count];
+            String[] splitArray;
+            splitArray = myArray[index].split("~");
+
+            System.out.println(splitArray[index] + " ");
+
         }
 
-
     }
-}
+    }
